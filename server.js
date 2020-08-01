e390bc95d64bf29d99d60e4879f35c1d90184a79
Rocky <rrsalerno21@gmail.com>
2020-08-01 07:21:40 -0700

    added options to mongoose.connect

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true,  useUnifiedTopology: true });
 
 // routes
 const apiRoutes = require("./routes/apiRoutes.js");
